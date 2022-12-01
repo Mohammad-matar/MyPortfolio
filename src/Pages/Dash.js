b61551e8fb7953816f9e25c16af917cc2e7b6c8e
Matarov <moe.matar1998@gmail.com>
2022-12-01 13:01:29 +0200

    deploy 002

--- src/Pages/Dash.js
+++ src/Pages/Dash.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Experience from '../Dashboard/Experience';
-import Personal from "../Dashboard/Personal";
-import Skills from '../Dashboard/Skills';
+// import Experience from '../Dashboard/Experience';
+// import Personal from "../Dashboard/Personal";
+// import Skills from '../Dashboard/Skills';
 
 export default function Dash() {
   return (
     <div>
-      <Personal />
+      {/* <Personal />
       <Skills />
-      <Experience />
+      <Experience /> */}
     </div>
   );
 }
